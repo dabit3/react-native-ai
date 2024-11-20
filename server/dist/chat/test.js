b898fdcfc1364588248cbee218f18f493b64b5c7
nader dabit <dabit3@gmail.com>
2024-11-20 11:05:56 -0600

    update .gitignore

--- server/dist/chat/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = void 0;
-async function test(req, res) {
-    res.send('hello world');
-}
-exports.test = test;
--- server/dist/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.baseHeaders = void 0;
-exports.baseHeaders = {
-    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-};
